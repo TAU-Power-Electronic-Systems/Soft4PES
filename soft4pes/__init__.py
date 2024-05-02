43f1dd5bc0595aa9aab854fa5f8cc5c384b6e500
artturuusila <arttu.ruusila@tuni.fi>
2024-05-02 11:58:32 +0300

    Add RL-grid model and conversions

--- soft4pes/__init__.py
+++ soft4pes/__init__.py
@@ -1,5 +1,7 @@
 from soft4pes import model
+from soft4pes import util
 
 __all__ = [
     "model",
+    "util",
 ]
--- soft4pes/model/__init__.py
+++ soft4pes/model/__init__.py
@@ -1,5 +1,7 @@
 from soft4pes.model import conv
+from soft4pes.model import grid
 
 __all__ = [
     "conv",
+    "grid",
 ]
