0687d7878ce9b34c8453cf33e97a15a93955cfd8
Babalou Milad <milad.babalou@aalto.fi>
2024-08-17 10:23:20 +0300

    create grid_LCL_filter.py

--- soft4pes/model/grid/__init__.py
+++ soft4pes/model/grid/__init__.py
@@ -5,9 +5,11 @@ Grid models.
 from soft4pes.model.grid.base_grid import BaseGrid
 from soft4pes.model.grid.rl_grid import RLGrid
 from soft4pes.model.grid.rl_grid_param import RLGridParameters
+from soft4pes.model.grid.grid_LCL_filter import GridLCLFilter
 
 __all__ = [
     "BaseGrid",
     "RLGrid",
     "RLGridParameters",
+    "GridLCLFilter",
 ]
