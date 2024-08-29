f25bb6ca972b4f117d93c3a26030a1cf88a0cb30
artturuusila <arttu.ruusila@tuni.fi>
2024-08-29 16:07:34 +0300

    Corrected and cleaned carriers

--- soft4pes/sim/__init__.py
+++ soft4pes/sim/__init__.py
@@ -1,4 +1,7 @@
+from soft4pes.sim.carrier_pwm import CarrierPWM
 from soft4pes.sim.simulation import Simulation
-from soft4pes.sim.carrier_pwm import SimulationPWM
 
-__all__ = ["Simulation", "SimulationPWM"]
+__all__ = [
+    "CarrierPWM",
+    "Simulation",
+]
