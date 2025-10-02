6d090bcfe201b3d6c29889e3e13b8e2ebb447bd4
Babalou Milad <milad.babalou@aalto.fi>
2025-10-02 14:57:07 +0300

    Create LFilterParameters Class

---
+++ soft4pes/model/grid/l_filter_param.py
@@ -0,0 +1,46 @@
+"""
+Parameters for an L-filter.
+"""
+
+
+class LFilterParameters():
+    """
+    Parameters for an LCL-filter.
+
+    Parameters
+    ----------
+    L_fc_SI : float
+        Inductance of the converter side filter inductor [H].
+    base : base value object
+        Base values.
+    L_fg_SI : float, optional
+        Inductance of the grid side filter inductor [H].
+    R_fc_SI : float, optional
+        Resistance of the converter side filter inductor [Ohm].
+    R_fg_SI : float, optional
+        Resistance of the grid side filter inductor [Ohm].
+
+    Attributes
+    ----------
+    X_fc : float
+        Reactance of the converter side filter inductor [p.u.].
+    R_fc : float
+        Resistance of the converter side filter inductor [p.u.].
+    X_fg : float
+        Reactance of the grid side filter inductor [p.u.].
+    R_fg : float
+        Resistance of the grid side filter inductor [p.u.].
+    base : base value object
+    """
+
+    def __init__(self,
+                 L_fc_SI,
+                 base,
+                 L_fg_SI=0,
+                 R_fc_SI=0,
+                 R_fg_SI=0):
+        self.X_fc = L_fc_SI / base.L
+        self.R_fc = R_fc_SI / base.Z
+        self.X_fg = L_fg_SI / base.L
+        self.R_fg = R_fg_SI / base.Z
+        self.base = base
