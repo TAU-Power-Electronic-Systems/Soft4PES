a1ca8cf9f1d504cee342656a99b3cde9e9229122
Babalou Milad <milad.babalou@aalto.fi>
2025-10-02 20:55:24 +0300

    Grid-following L Filter

---
+++ examples/grid/plotters/plot_grid_following_ctr.py
@@ -0,0 +1,78 @@
+"""
+Plotter for grid-following control examples.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from soft4pes.utils import alpha_beta_2_dq
+
+def plot_gfl_example(data, t_start=0):
+    """
+    Plot the grid-following control example results.
+
+    Parameters
+    ----------
+    data : SimpleNamespace
+        The simulation data containing system and control data.
+    t_start : float, optional
+        The start time for plotting to exclude initial transients. Default is 0.
+    """
+    # Adjust the font size for all text elements
+    plt.rcParams.update({'font.size': 14})
+
+    # Extract time vectors and adjust for start time
+    t_sim = data.sys.t - t_start
+    t_ctr = data.ctr.t - t_start
+
+    # Extract system state variables
+    x = data.sys.x
+    i_conv = x[:, 0:2]  # Converter currents
+    ig = i_conv # Grid currents (L filter)
+    i_conv_ref_dq = data.ctr.LConvCurrCtr.input.ig_ref_dq  # Current reference in dq frame
+    vg = data.sys.vg  # Grid voltages
+
+    # Calculate power and capacitor voltage magnitude
+    P = vg[:, 0] * ig[:, 0] + vg[:, 1] * ig[:, 1]
+    Q = vg[:, 1] * ig[:, 0] - vg[:, 0] * ig[:, 1]
+    P_ref = data.ctr.GridCurrRefGen.input.P_ref
+    Q_ref = data.ctr.GridCurrRefGen.input.Q_ref
+    
+    # Calculate the transformation angle
+    theta = np.arctan2(vg[:, 1], vg[:, 0])
+    # Get dq frame current (converter current equals grid current due to lack of a filter)
+    i_conv_dq = np.empty_like(i_conv)
+    for k in range(i_conv.shape[0]):
+     i_conv_dq[k, :] = alpha_beta_2_dq(i_conv[k, :], theta[k])
+
+    # Define plot limits
+    x_lim = [0, t_sim[-1]]
+
+    # Create subplots
+    fig, axs = plt.subplots(2, 1, figsize=(10, 6))
+
+    # Plot power
+    axs[0].plot(t_sim, P, color='blue', label='P')
+    axs[0].plot(t_ctr, P_ref, '--', color='blue', label='P_ref')
+    axs[0].plot(t_sim, Q, color='red', label='Q')
+    axs[0].plot(t_ctr, Q_ref, '--', color='red', label='Q_ref') 
+    axs[0].set_xlim(x_lim)
+    axs[0].set_xticklabels([])
+    axs[0].set_ylabel('P and Q [p.u.]')
+    axs[0].grid(True)
+    axs[0].legend()
+
+    # Plot capacitor voltage magnitude
+    axs[1].plot(t_sim, i_conv_dq[:, 0], color='blue', label='id')
+    axs[1].plot(t_ctr, i_conv_ref_dq[:, 0], '--', color='blue', label='i_ref_d')
+    axs[1].plot(t_sim, i_conv_dq[:, 1], color='red', label='iq')
+    axs[1].plot(t_ctr, i_conv_ref_dq[:, 1], '--', color='red', label='i_ref_q')
+    axs[1].set_xlim(x_lim)
+    axs[1].set_xlabel('Time [s]')
+    axs[1].set_ylabel('i_dq [p.u.]')
+    axs[1].grid(True)
+    axs[1].legend()
+
+    # Align y-axis labels and adjust layout
+    fig.align_ylabels()
+    plt.tight_layout()
+    plt.show()
--- soft4pes/control/lin/l_conv_curr_ctr.py
+++ soft4pes/control/lin/l_conv_curr_ctr.py
@@ -23,6 +23,8 @@ class LConvCurrCtr(Controller):
     ----------
     u_ii_comp : complex
         Integrator state for the converter voltage reference in the dq-frame.
+    uc_km1_dq : complex
+        Previous converter voltage reference in dq frame [p.u.].     
     v_conv_kp1_comp : complex
         Next converter voltage reference in the dq-frame.
     u_km1_abc : ndarray (3,)
@@ -36,6 +38,7 @@ class LConvCurrCtr(Controller):
     def __init__(self, sys):
         super().__init__()
         self.u_ii_comp = complex(0, 0)
+        self.uc_km1_dq = complex(0, 0)
         self.v_conv_kp1_comp = complex(0, 0)
         self.u_km1_abc = np.array([0, 0, 0])
         self.sys = sys
@@ -56,21 +59,36 @@ class LConvCurrCtr(Controller):
         # Closed-loop controller bandwidth (10x crossover frequency)
         alpha_c = 2 * np.pi / 10 / Ts_pu
 
-        # Consider delta due to considering delay
+        # Consider delta equals to one due to not considering delay
         delta = 1
 
-        # Controller parameters
+        # Coefficients
+        phi = np.exp((-self.sys.par.R_fc / self.sys.par.X_fc) * Ts_pu) * delta
+        landa = (delta - phi) / self.sys.par.R_fc
+
+        # The closed-loop pole locations
+        p_1 = 0
+        p_2 = np.exp(-alpha_c * Ts_pu)
+        p_3 = p_2
+
+        # Coefficients
+        k_2 = -p_1 - p_2 - p_3 + phi + 1
+        k_1 = (p_1 * p_2 + p_1 * p_3 + p_2 * p_3 + k_2 * phi + k_2 -
+               phi) / landa
+
+        # State feedback gain
+        K_i = np.array([k_1, 0, k_2 * 0])
+
         # Integral gain
-        k_i = alpha_c^2 * self.sys.par.X_fc
-        # Proportional gain
-        k_p = (2*alpha_c - 1j*self.sys.par.wg)*self.sys.par.X_fc
+        k_ii = (-p_1 * p_2 * p_3 + k_1 * landa - k_2 * phi) / landa
+
         # Feedforward gain
-        k_t = alpha_c * self.sys.par.X_fc
+        k_ti = k_ii / (1 - p_3)
 
         self.ctr_pars = SimpleNamespace(delta=delta,
-                                        k_p=k_p,
-                                        k_i=k_i,
-                                        k_t=k_t)
+                                        K_i=K_i,
+                                        k_ii=k_ii,
+                                        k_ti=k_ti)     
 
     def execute(self, sys, kTs):
         """
@@ -91,32 +109,39 @@ class LConvCurrCtr(Controller):
 
         vg = sys.get_grid_voltage(kTs)
 
-        # Get the grid-voltage angle
+        # Calculate the transformation angle
         theta = np.arctan2(vg[1], vg[0])
 
         # Get the reference for current step
         i_conv_ref_comp = complex(*self.input.ig_ref_dq)
 
-        # Get the converter current in dq-frame
+        # Get dq frame current (converter current equals grid current due to lack of a filter)
         i_conv_comp = complex(*alpha_beta_2_dq(sys.i_conv, theta))
 
-        v_conv_ref_unlim_comp = self.ctr_pars.k_t*i_conv_ref_comp - self.ctr_pars.k_p*i_conv_comp + self.u_ii_comp
+        # As the filter is not concidered, the filter capacitor voltage is assumed to be the same
+        # as the grid voltage after the grid indutance.
+        uf_dq = complex(*alpha_beta_2_dq(vg-((self.sys.par.Rg+self.sys.par.Xg)* sys.i_conv), theta))
+
+        x_lc = np.array([i_conv_comp, uf_dq, self.uc_km1_dq])
+
+        # State space controller with anti-windup in dq frame
+        v_conv_ref_unlim_comp = (self.ctr_pars.k_ti * i_conv_ref_comp) - np.dot(
+            self.ctr_pars.K_i, x_lc) + self.u_ii_comp
 
         # Limiting the converter voltage reference
         v_conv_ref_comp = magnitude_limiter(v_conv_ref_unlim_comp,
-                                            sys.conv.v_dc / 2)
+                                           sys.conv.v_dc / 2)
 
-        self.v_conv_kp1_comp = v_conv_ref_comp
-
-        self.u_ii_comp += self.ctr_pars.k_ii * (
-            ((v_conv_ref_comp - v_conv_ref_unlim_comp) / self.ctr_pars.k_t) +
-            (i_conv_ref_comp - i_conv_comp))
-
+        self.u_ii_comp += self.ctr_pars.k_ii * ((
+            (v_conv_ref_comp - v_conv_ref_unlim_comp) / self.ctr_pars.k_ti) +
+                                              (i_conv_ref_comp - i_conv_comp))
+        
         # Get the modulating signal and Hold it for one cycle before sending it out from the
         # controller
         u_abc = self.u_km1_abc
         v_conv_ref_dq = np.array([v_conv_ref_comp.real, v_conv_ref_comp.imag])
         v_conv_ref = dq_2_alpha_beta(v_conv_ref_dq, theta)
+        self.uc_km1_dq = self.ctr_pars.delta * v_conv_ref_comp
         self.u_km1_abc = get_modulating_signal(v_conv_ref, sys.conv.v_dc)
         self.output = SimpleNamespace(u_abc=u_abc)
 
