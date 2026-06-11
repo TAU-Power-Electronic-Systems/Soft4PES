Remove plotting, fix angle propagation, fix PCC calculation

soft4pes/control/lin/lcl_grid_curr_ctr_wacfb.py
@@ -157,10 +157,16 @@ class LCLGridCurrCtrWACFB(Controller):
 
     def execute(self, sys, kTs):
 
-        theta = self.input.theta
+        # Get the transformation angle from the outer loop. If not available, use the grid voltage
+        # angle.
+        if getattr(self.input, "theta", None) is not None:
+            theta = self.input.theta
+        else:
+            vg = sys.get_grid_voltage(kTs)
+            theta = np.arctan2(vg[1], vg[0])
 
         # Get the grid current reference
-        ig_ref_dq = self.input.ig_ref_dq
+        ig_ref_dq = alpha_beta_2_dq(self.input.ig_ref, theta)
         ig_dq = alpha_beta_2_dq(sys.ig, theta)
 
         # WAC feedback: iF_dq = W_D_if * W_Q * ig_dq

soft4pes/control/mpc/algorithms/lcl_grid_vc_ctr.py
@@ -91,20 +91,16 @@ class LCLGridVcCtr(MPCBase, Controller):
 
         self.get_ctr_state_space(sys, self.Ts)
 
-        # Get the grid voltage and save it for future use
-        vg = sys.get_grid_voltage(kTs)
-        Ts_pu = self.Ts * sys.base.w
-        d_pred = self.make_disturbance_vector(sys.par.wg, Ts_pu, vg)
-
-        # Get the reference at step k
-        vc_ref_dq = self.input.vc_ref_dq
-
-        # Get the grid-voltage angle and calculate the reference in alpha-beta frame
-        theta = np.arctan2(vg[1], vg[0])
-        vc_ref = dq_2_alpha_beta(vc_ref_dq, theta)
+        # Get the reference for current step
+        vc_ref = self.input.vc_ref
 
         # Predict the output (capacitor voltage) reference within the horizon
+        Ts_pu = self.Ts * sys.base.w
         y_ref_pred = self.make_reference_vector(sys.par.wg, Ts_pu, vc_ref)
+
+        # Predict the grid voltage disturbance over the horizon
+        vg = sys.get_grid_voltage(kTs)
+        d_pred = self.make_disturbance_vector(sys.par.wg, Ts_pu, vg)
 
         # Solve the control problem
         u_abc = self.solver(sys, self, y_ref_pred, d_pred)

soft4pes/model/grid/rl_grid_lcl_filter.py
@@ -124,23 +124,24 @@ class RLGridLCLFilter(RLGrid):
 
         return SimpleNamespace(F=F, G=G, P=P)
 
-    def get_pcc_voltage(self, vg):
+    def get_pcc_voltage(self, kTs):
         """
         Get the voltage at the point of common coupling (PCC).
         
         Parameters
         ----------
-        vg : 1 x 2 ndarray of floats
-            Grid voltage [p.u.].
+        kTs : float
+            Current discrete time instant [s].
 
         Returns
         -------
         1 x 2 ndarray of floats
-            Voltage at the point of common coupling (PCC) in alpha-beta frame [p.u.].
+            Voltage at the point of common coupling (PCC) [p.u.].
         """
 
         ig = self.ig
         J = np.array([[0, -1], [1, 0]])
+        vg = self.get_grid_voltage(kTs)
         v_pcc = vg + self.par.Rg * ig + self.par.Xg * J.dot(ig)
 
         return v_pcc